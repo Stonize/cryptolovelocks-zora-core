f8cfca8adab6b79922ae871e040cdcfa6a6f8f94
Michele Mastrogiovanni <michele.mastrogiovanni@gmail.com>
2021-05-25 18:19:25 +0200

    Removed permit and mintWithSig interface and implementations

--- contracts/Media.sol
+++ contracts/Media.sol
@@ -99,10 +99,6 @@ contract Media is IMedia, ERC721Burnable, ReentrancyGuard {
     // Mapping from contentHash to bool
     mapping(bytes32 => bool) private _contentHashes;
 
-    //keccak256("Permit(address spender,uint256 tokenId,uint256 nonce,uint256 deadline)");
-    bytes32 public constant PERMIT_TYPEHASH =
-        0x49ecf333e5b8c95c40fdafc95c1ad136e8914a8fb55e9dc8bb01eaa83a2df9ad;
-
     // Mapping from address to token id to permit nonce
     mapping(address => mapping(uint256 => uint256)) public permitNonces;
 
